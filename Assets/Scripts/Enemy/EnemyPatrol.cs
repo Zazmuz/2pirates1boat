c8bb714f8dc1517b80df34ef06432b8e414fdfbe
Bob Rolf <143784071+BobRolf@users.noreply.github.com>
2025-02-22 16:50:37 +0100

    Added_crabs
    Crabs+patrolingscript

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float speed;
+    public int patrolDestination;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(patrolDestination == 0){
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, speed * Time.deltaTime);
+            if(Vector2.Distance(transform.position, patrolPoints[0].position) < 0.5f){
+                transform.localScale = new Vector2(-1,1);
+                patrolDestination = 1;
+            }
+        }
+        if(patrolDestination == 1){
+        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, speed * Time.deltaTime);
+            if(Vector2.Distance(transform.position, patrolPoints[1].position) < 0.5f){
+                transform.localScale = new Vector2(1,1);
+                patrolDestination = 0;
+            }
+        }
+    }   
+}
