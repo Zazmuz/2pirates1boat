9c359f5a12f7f3fb3a0e22d6e392d10d1865cb27
Zazmuz <rasmuswario@gmail.com>
2025-03-05 20:16:54 +0100

    fixed respawning

--- Assets/Scripts/Input/PlayerDeath.cs
+++ Assets/Scripts/Input/PlayerDeath.cs
@@ -1,11 +1,38 @@
 using UnityEngine;
 public class PlayerDeath : MonoBehaviour{
     public GameInformation gameInformation;
-    void OnTriggerEnter2D(Collider2D collision){
-        if (collision.CompareTag("Death")){
-            Destroy(gameObject);
-            gameInformation.RemovePlayer(gameObject.GetComponent<GameObject>());
+    public Transform respawnSpot;
+    private Transform boat;
+    void Awake()
+    {
+        respawnSpot = GameObject.FindWithTag("Respawn").transform;
+        boat = GameObject.FindWithTag("Ship").transform;
+        
+        if (respawnSpot == null)
+        {
+            Debug.LogError("Respawn spot not found!");
+        }
+
+        if (boat == null)
+        {
+            Debug.LogError("Boat not found!");
         }
     }
-        
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.CompareTag("Death")){
+            if (respawnSpot == null || boat == null)
+            {
+                Debug.LogError("Respawn spot or Boat not found!");
+                Destroy(gameObject);
+                gameInformation.RemovePlayer(gameObject);
+                return;
+            }
+            else
+            {
+                Vector2 boatOffset = respawnSpot.position - boat.position;
+                transform.position = (Vector2)boat.position + boatOffset;
+            }
+        }
+    }
 }
