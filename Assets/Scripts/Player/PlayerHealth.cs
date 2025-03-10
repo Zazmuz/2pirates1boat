87da0dd8b4acc3cdfd9480c94b16ac1165cd7df6
Zazmuz <rasmuswario@gmail.com>
2025-03-10 20:19:24 +0100

    respawn if crab

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -10,10 +10,25 @@ public class PlayerHealth : MonoBehaviour
     [Range(1,100)] public int maxhealth = 100;
     
     public int currentHealth;
+    private Transform respawnSpot;
+    private Transform boat;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxhealth;
+        respawnSpot = GameObject.FindWithTag("Respawn").transform;
+        boat = GameObject.FindWithTag("Ship").transform;
+
+        if (respawnSpot == null)
+        {
+            Debug.LogError("Respawn spot not found!");
+        }
+
+        if (boat == null)
+        {
+            Debug.LogError("Boat not found!");
+        }
     }
 
     public void TakeDamage(int damage)
@@ -21,8 +36,21 @@ public class PlayerHealth : MonoBehaviour
         currentHealth -= damage;
         if (currentHealth <= 0)
         {
-            Destroy(gameObject);
-            gameInformation.RemovePlayer(gameObject.GetComponent<GameObject>());
+            RespawnPlayer();
+        }
+    }
+
+    private void RespawnPlayer()
+    {
+        if (respawnSpot != null && boat != null)
+        {
+            Vector2 boatOffset = respawnSpot.position - boat.position;
+            transform.position = (Vector2)boat.position + boatOffset;
+            currentHealth = maxhealth; // Reset health
+        }
+        else
+        {
+            Debug.LogError("Respawn spot or Boat not found!");
         }
     }
 }
