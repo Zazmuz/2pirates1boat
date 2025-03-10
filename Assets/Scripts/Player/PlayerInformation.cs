e068fc78649e431529b034190637eafdb8ecc130
Bob Rolf <143784071+BobRolf@users.noreply.github.com>
2025-03-10 15:33:17 +0100

    Revert "lol commitment issueies"
    This reverts commit 35c45c486dd8e6b2bbcb9dddf1fd15bc7c3c6fae.

--- Assets/Scripts/Player/PlayerInformation.cs
+++ Assets/Scripts/Player/PlayerInformation.cs
@@ -8,34 +8,8 @@ public class PlayerInformation : ScriptableObject
     public Animation idle;
     public Animation running;
 
-    [Header("Position")]
+    [Header("Gameplay and position")]
     public float posX;
     public float posY;
 
-    [Header("Gameplay")]
-    private float health;
-    private float vitaminC;
-    
-    public void ModifyHealth(float amount)
-    {
-        health -= amount;
-        health = Mathf.Clamp(health, 0, 100);
-    }
-    public void ModifyVitaminC(float amount)
-    {
-        vitaminC += amount;
-        vitaminC = Mathf.Clamp(vitaminC, 0, 100);
-    }
-    public void ResetStats()
-    {
-        health = 100;
-        vitaminC = 100;
-    }
-    public float GetHealth(){
-        return health;
-    }
-    public float GetVitaminC(){
-        return vitaminC;
-    }
 }
-
