35c45c486dd8e6b2bbcb9dddf1fd15bc7c3c6fae
Bob Rolf <143784071+BobRolf@users.noreply.github.com>
2025-03-10 11:11:21 +0100

    lol commitment issueies

--- Assets/Scripts/Player/PlayerInformation.cs
+++ Assets/Scripts/Player/PlayerInformation.cs
@@ -8,8 +8,34 @@ public class PlayerInformation : ScriptableObject
     public Animation idle;
     public Animation running;
 
-    [Header("Gameplay and position")]
+    [Header("Position")]
     public float posX;
     public float posY;
 
+    [Header("Gameplay")]
+    private float health;
+    private float vitaminC;
+    
+    public void ModifyHealth(float amount)
+    {
+        health -= amount;
+        health = Mathf.Clamp(health, 0, 100);
+    }
+    public void ModifyVitaminC(float amount)
+    {
+        vitaminC += amount;
+        vitaminC = Mathf.Clamp(vitaminC, 0, 100);
+    }
+    public void ResetStats()
+    {
+        health = 100;
+        vitaminC = 100;
+    }
+    public float GetHealth(){
+        return health;
+    }
+    public float GetVitaminC(){
+        return vitaminC;
+    }
 }
+
